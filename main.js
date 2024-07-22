686db6621b7d46da6e1a69767965ed356bbf7157
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-22 19:51:01 +0300

    step-5

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-const location_1 = 3
-const location_2 = 4
-const location_3 = 5
+// const location_1 = 3
+// const location_2 = 4
+// const location_3 = 5
 
-const guess = 4
-if (guess === location_1 || guess === location_2 || guess === location_3) {
-    alert("HIT!")
-} else {
-    alert("MISS!")
-  }
+// const guess = 4
+// if (guess === location_1 || guess === location_2 || guess === location_3) {
+//     alert("HIT!")
+// } else {
+//     alert("MISS!")
+//   }
 
-  if(guess === location_1){
-    alert("HIT!")
-  } else if (guess === location_2) {
-    alert("HIT!")
-  } else if (guess === location_3) {
-    alert("HIT!")
-  }
+//   if(guess === location_1){
+//     alert("HIT!")
+//   } else if (guess === location_2) {
+//     alert("HIT!")
+//   } else if (guess === location_3) {
+//     alert("HIT!")
+//   }
 
   const inStock = true
   const onSale = true
 
-  ifinStock === true){
+  if (inStock === true){
     if(onSale === true){
-        
+        alert("Buy!!!")
     }
   } 
