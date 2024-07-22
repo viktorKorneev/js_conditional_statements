2ecc898d2752e373021cb9c72bc0f1d40bcd1f80
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-22 19:37:02 +0300

    step-1

---
+++ main.js
@@ -0,0 +1,10 @@
+const location_1 = 3
+const location_2 = 4
+const location_3 = 5
+
+const guess = 4
+if (guess === location1 || guess === location2 || guess === location3) {
+    alert("HIT!")
+} else {
+    alert("MISS!")
+  }
